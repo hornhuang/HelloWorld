5179b198a22859e50fd3dcfbff6b8570d33211e0
huangyuanhao <307975180@qq.com>
2019-04-01 22:45:37 +0800

    Fish:
    牛顿插值公式开始

---
+++ app/src/main/java/com/example/helloworld/utils/Point.java
@@ -0,0 +1,30 @@
+package com.example.helloworld.utils;
+
+public class Point {
+
+    private int x;
+    private double y;
+
+    public Point(int x, double y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+}
